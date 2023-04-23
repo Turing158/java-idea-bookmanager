01e78d64ea4e1d69615346926077e75f8594fa62
test <15818961209@163.com>
2023-04-23 14:14:48 +0800

    test

--- test/MainTests.java
+++ test/MainTests.java
@@ -8,7 +8,7 @@ import java.util.Properties;
 public class MainTests {
     @Test
     public void firstTest(){
-        System.out.println("hello test");
+        System.out.println("hello test!");
         try{
             Properties properties=new Properties();  // 用/文件名， / 表示根目录
             InputStream input=MainTests.class.getResourceAsStream("/application.properties");
